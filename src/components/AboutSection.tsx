d5eb94807201f0678b4b96481aa69f9a47e5bd7b
Kirubaraj Manimathavan <kirubarajmathavan@gmail.com>
2024-08-12 22:15:30 -0700

    Changed order of skill categories

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -29,11 +29,11 @@ const AboutSection = () => {
           <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
         </h1>
         <div className="space-y-10 md:space-y-6 md:p-4">
-          <div>
-            <h1 className="text-2xl font-bold mb-4">Web and Mobile</h1>
+        <div>
+            <h1 className="text-2xl font-bold mb-4">AI</h1>
             {/* <hr className="w-full h-1 my-2 bg-gray-300 border-0 rounded"></hr> */}
             <div className="flex flex-wrap">
-              {webMobileSkills.map((skill, idx) => (
+              {aiSkills.map((skill, idx) => (
                 <p key={idx} className="bg-gray-200 px-4 py-2 mr-2 mb-2 text-gray-500 rounded font-semibold">
                   {skill}
                 </p>
@@ -52,10 +52,10 @@ const AboutSection = () => {
             </div>
           </div>
           <div>
-            <h1 className="text-2xl font-bold mb-4">AI</h1>
+            <h1 className="text-2xl font-bold mb-4">Web and Mobile</h1>
             {/* <hr className="w-full h-1 my-2 bg-gray-300 border-0 rounded"></hr> */}
             <div className="flex flex-wrap">
-              {aiSkills.map((skill, idx) => (
+              {webMobileSkills.map((skill, idx) => (
                 <p key={idx} className="bg-gray-200 px-4 py-2 mr-2 mb-2 text-gray-500 rounded font-semibold">
                   {skill}
                 </p>
